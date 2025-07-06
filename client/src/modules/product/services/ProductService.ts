37f9c164bbe90c92ce82d74c6a19a62ba26cf45d
RybalkaOleksandr <o.rybalka@sysgears.com>
2025-07-06 16:45:53 +0300

    Fixed relaoding

--- client/src/modules/product/services/ProductService.ts
+++ client/src/modules/product/services/ProductService.ts
@@ -1,9 +1,16 @@
+import { IOwnApiOptions } from "@modules/common/types";
 import { ownApi } from "@utils/axios";
+import { getAxiosCacheOptions } from "@utils/axios/getAxiosCacheOptions";
 
 class ProductService {
-  public getStripeProducts = async (): Promise<any> => {
+  public getStripeProducts = async (
+    ownApiOptions?: IOwnApiOptions
+  ): Promise<any> => {
     try {
-      const { data } = await ownApi.get("stripe/products");
+      const { data } = await ownApi.get("stripe/products", {
+        ...getAxiosCacheOptions(ownApiOptions),
+        id: "stripe/products",
+      });
 
       return data;
     } catch (error: any) {
--- client/src/modules/product/stores/ProductsStore.ts
+++ client/src/modules/product/stores/ProductsStore.ts
@@ -2,12 +2,13 @@ import { observable, makeObservable, runInAction } from "mobx";
 import { IProduct } from "../types";
 import { productService } from "../services";
 import BaseStore from "@utils/BaseStore";
+import { IExecuteStore } from "@modules/common/types";
 
 class ProductsStore extends BaseStore {
   public products: IProduct[] | null = null;
 
-  public onExecute = async () => {
-    const response = await productService.getStripeProducts();
+  public onExecute = async ({ ownApiOptions }: IExecuteStore) => {
+    const response = await productService.getStripeProducts(ownApiOptions);
 
     runInAction(() => {
       this.products = response;
