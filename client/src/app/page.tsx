795af0f104d855f51e3bd38d1b5f9b59830caa78
RybalkaOleksandr <o.rybalka@sysgears.com>
2025-07-11 17:14:17 +0300

    Added total amount component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,29 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
 import styles from "./page.module.scss";
-import { commonStore } from "@modules/common/stores";
 import { Checkout, ProductList } from "@modules/product/components";
-import { newOrderStore } from "@modules/product/stores";
 import CustomCheckout from "@modules/payment/components/CustomCheckout";
+import TotalAmount from "@modules/product/components/TotalAmount";
 
 export default function Home() {
-  useEffect(() => {
-    commonStore.reload({
-      data: {
-        pagination: {
-          pageSize: 1,
-          current: 1,
-        },
-      },
-    });
-  }, []);
-
   return (
     <div className={styles.wrapper}>
-      <div>Total Amount: ${newOrderStore.total.toFixed(2)}</div>
-
       <ProductList />
+      <TotalAmount />
       <Checkout />
       <CustomCheckout />
     </div>
