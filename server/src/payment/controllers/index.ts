610ab0d936c382c0205c66ea62b6eaed2513f660
RybalkaOleksandr <o.rybalka@sysgears.com>
2025-08-25 11:55:59 +0300

    Cleaned up code

--- server/src/payment/controllers/index.ts
+++ server/src/payment/controllers/index.ts
@@ -1,12 +1,10 @@
 import { StripeCommonController } from './stripe-common.controller';
-import { CreateStripeCheckoutSessionController } from './stripe.controller';
 import { PaymentIntentController } from './payment-intent.controller';
 import { SetupIntentController } from './setup-intent.controller';
 import { CheckoutSessionController } from './checkout-session.controller';
 
 export const paymentModuleControllers = [
   StripeCommonController,
-  CreateStripeCheckoutSessionController,
   PaymentIntentController,
   SetupIntentController,
   CheckoutSessionController,
