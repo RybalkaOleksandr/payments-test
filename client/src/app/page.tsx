bff0d3b210d7e2062d08e055bfe1409645f46234
RybalkaOleksandr <o.rybalka@sysgears.com>
2025-10-05 17:15:23 +0300

    Separate whole flow into steps

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,27 +1,7 @@
 "use client";
 
-import { CheckoutSession, PaymentMethods } from "@modules/payment/components";
-import styles from "./page.module.scss";
-import { ProductList } from "@modules/product/components";
-import TotalAmount from "@modules/product/components/TotalAmount";
-import CustomCheckoutBtn from "@modules/payment/components/CustomCheckoutBtn";
-import SelectUser from "@modules/user/components/SelectUser";
-import PurchaseSubscriptionBtn from "@modules/payment/components/PurchaseSubscriptionBtn";
-import AddPaymentDetailsBtn from "@modules/payment/components/AddPaymentDetailsBtn";
-import GooglePayBtn from "@modules/payment/components/GooglePayBtn";
+import MainSteps from "@modules/common/components/MainSteps";
 
 export default function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <SelectUser />
-      <PaymentMethods />
-      <ProductList />
-      <TotalAmount />
-      <CheckoutSession />
-      <CustomCheckoutBtn />
-      <AddPaymentDetailsBtn />
-      <PurchaseSubscriptionBtn />
-      <GooglePayBtn />
-    </div>
-  );
+  return <MainSteps />;
 }
