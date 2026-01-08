7c445fb8abd25f6a7d262dc387455792067929af
RybalkaOleksandr <o.rybalka@sysgears.com>
2026-01-08 15:34:28 +0200

    base

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// test
+// base
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
